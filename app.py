7acd8991e5b23a2d67d5065a29baa8ed2e64d453
PWU3U <m.garafutdinov2004@gmail.com>
2025-05-31 14:10:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 pages = {
-    "Анализ и модель" analysis_and_model.py,
-    "Презентация" presentation.py
+    "Анализ и модель": "analysis_and_model.py",
+    "Презентация": "presentation.py"
 }
 
-st.sidebar.title(Навигация)
-selection = st.sidebar.radio(Страницы, list(pages.keys()))
+st.sidebar.title("Навигация")
+selection = st.sidebar.radio("Страницы", list(pages.keys()))
 
-with open(pages[selection]) as f
+with open(pages[selection]) as f:
     exec(f.read(), globals())
